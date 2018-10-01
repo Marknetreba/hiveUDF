b1c79b5557d4b276c5d9b801dca75e9df823ead7
mnetreba <mark.netreba@griddynamics.com>
2018-10-01 14:14:51 +0400

    refactoring

--- src/main/java/udf/JavaUDF.java
+++ src/main/java/udf/JavaUDF.java
@@ -3,10 +3,13 @@ package udf;
 import org.apache.commons.net.util.SubnetUtils;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class JavaUDF extends UDF {
 
-    public String[] checkIP(String network) {
+    public ArrayList checkIP(String network) {
         SubnetUtils utils = new SubnetUtils(network);
-        return utils.getInfo().getAllAddresses();
+        return new ArrayList(Arrays.asList(utils.getInfo().getAllAddresses()));
     }
 }
